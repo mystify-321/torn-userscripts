8de18597fc3ccb5d48af9a5ea0e16606327cdffe
Jens Brimfors <jens.alexander.brimfors@wolt.com>
2026-02-18 11:32:43 +0100

    fix weav3r url matcher

--- weav3r-ff.js
+++ weav3r-ff.js
@@ -4,7 +4,7 @@
 // @version      2026-02-17
 // @description  try to take over the world!
 // @author       You
-// @match        https://weav3r.dev/item/*
+// @match        https://weav3r.dev/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weav3r.dev
 // @grant        GM_getValue
 // @grant        GM_setValue
